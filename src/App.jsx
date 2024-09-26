3f410727d6c606ad3c46312710c91fc4d33759c1
Darshanpro123 <darshanpro187@gmail.com>
2024-09-26 16:45:42 +0530

    add css style to list card box

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ function App() {
   console.log(data);
   return (
     <>
-      <h1>My Pop_up box</h1>
+      <div>
+        <h1>My Pop_Out_box</h1>
+        <div className="lists">
+          {data.map((qus, i) => {
+            return (
+              <>
+                {" "}
+                <div key={i} className="list">
+                  <div className="list-title">
+                    <span>{i + 1}.</span>
+                    <h2>{qus.title}</h2>
+                  </div>
+                  <p>{qus.text}</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
