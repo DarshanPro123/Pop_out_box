9f28198de3b5dae81af3d831fdafb05c6f66fa03
Darshanpro123 <darshanpro187@gmail.com>
2024-09-26 16:19:33 +0530

    add data from code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import data from "../public/data";
+
+function App() {
+  console.log(data);
+  return (
+    <>
+      <h1>My Pop_up box</h1>
+    </>
+  );
+}
+
+export default App;
